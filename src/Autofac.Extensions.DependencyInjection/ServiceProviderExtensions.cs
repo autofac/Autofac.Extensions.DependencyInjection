6cf1ca3500c04a842292d6d6335ae0a24b0e0b16
alsami <al.khatib.sami1990@gmail.com>
2019-08-12 10:55:48 +0200

    feat: add compatibility for `IHost` * Update global.json and change sdk-version to `3.0.100-preview8-013656` * Add `Microsoft.Extensions.Hosting.Abstractions` with version `3.0.0-preview8.19405.4` to src-project * Add `HostBuilderExtensions` and tests to register `AutofacServiceProviderFactory` with a convenient extension method * Expose `ILifetimeScope` from `AutofacServiceProvider` and add `ServiceProviderExtensions` and tests to get a reference to the root-container using interface `IServiceProvider` * Remove `ServiceCollectionExtensions` and tests * Remove target-framework `netstandard1.1` from src-project * Remove explicit reference to `Microsoft.Extensions.DependencyInjection.Abstractions` from test-project * Update test-project to use `netcoreapp3.0` * Update `Microsoft.Extensions.DependencyInjection.Abstractions` to `3.0.0-preview8.19405.4` * Update `Microsoft.SourceLink.GitHub` to `1.0.0-beta2-19367-01` * Update `README.md` to reflect latest usage

--- src/Autofac.Extensions.DependencyInjection/ServiceProviderExtensions.cs
+++ src/Autofac.Extensions.DependencyInjection/ServiceProviderExtensions.cs
@@ -1,5 +1,5 @@
 ﻿// This software is part of the Autofac IoC container
-// Copyright © 2017 Autofac Contributors
+// Copyright © 2019 Autofac Contributors
 // https://autofac.org
 //
 // Permission is hereby granted, free of charge, to any person
@@ -24,24 +24,28 @@
 // OTHER DEALINGS IN THE SOFTWARE.
 
 using System;
-using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace Autofac.Extensions.DependencyInjection
 {
     /// <summary>
-    /// Extension methods on <see cref="IServiceCollection"/> to register the <see cref="IServiceProviderFactory{TContainerBuilder}"/>.
+    /// Extension method on <see cref="IServiceProvider" /> to get the instance of <see cref="AutofacServiceProvider"/> when it has been registered with <see cref="IHost"/>.
+    /// <seealso cref="HostBuilderExtensions" />
     /// </summary>
-    public static class ServiceCollectionExtensions
+    public static class ServiceProviderExtensions
     {
         /// <summary>
-        /// Adds the <see cref="AutofacServiceProviderFactory"/> to the service collection.
+        /// Tries to cast the instance of <see cref="ILifetimeScope"/> from <see cref="AutofacServiceProvider"/> when possible.
         /// </summary>
-        /// <param name="services">The service collection to add the factory to.</param>
-        /// <param name="configurationAction">Action on a <see cref="ContainerBuilder"/> that adds component registrations to the container.</param>
-        /// <returns>The service collection.</returns>
-        public static IServiceCollection AddAutofac(this IServiceCollection services, Action<ContainerBuilder> configurationAction = null)
+        /// <param name="serviceProvider">The instance of <see cref="IServiceProvider"/>.</param>
+        /// <exception cref="InvalidOperationException">Throws an <see cref="InvalidOperationException"/> when instance of <see cref="IServiceProvider"/> can't be assigned to <see cref="AutofacServiceProvider"/>.</exception>
+        /// <returns>Returns the instance of <see cref="ILifetimeScope"/> exposed by <see cref="AutofacServiceProvider"/>.</returns>
+        public static ILifetimeScope GetAutofacRoot(this IServiceProvider serviceProvider)
         {
-            return services.AddSingleton<IServiceProviderFactory<ContainerBuilder>>(new AutofacServiceProviderFactory(configurationAction));
+            if (!(serviceProvider is AutofacServiceProvider autofacServiceProvider))
+                throw new InvalidOperationException($"Instance of {nameof(serviceProvider)} is not of type {nameof(AutofacServiceProvider)}");
+
+            return autofacServiceProvider.LifetimeScope;
         }
     }
 }
---
+++ test/Autofac.Extensions.DependencyInjection.Test/HostBuilderExtensionsTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Xunit;
+
+namespace Autofac.Extensions.DependencyInjection.Test
+{
+    public sealed class HostBuilderExtensionsTests
+    {
+        [Fact]
+        public void UseAutofacAutofacServiceProviderResolveable()
+        {
+            var host = Host.CreateDefaultBuilder(null)
+                .UseAutofac()
+                .Build();
+
+            Assert.IsAssignableFrom<AutofacServiceProvider>(host.Services);
+        }
+    }
+}
