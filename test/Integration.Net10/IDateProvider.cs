9c1c1ec9282eeffc1095d6952bbe19d56ac95037
Travis Illig <tillig@paraesthesia.com>
2025-11-19 14:24:09 -0800

    Fix namespaces for Net10 integration.

--- test/Integration.Net10/IDateProvider.cs
+++ test/Integration.Net10/IDateProvider.cs
@@ -1,9 +1,7 @@
 ﻿// Copyright (c) Autofac Project. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-using System;
-
-namespace Integration.Net8;
+namespace Integration.Net10;
 
 public interface IDateProvider
 {
