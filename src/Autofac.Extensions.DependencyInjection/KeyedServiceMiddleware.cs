b62123f3d471f8f17f04eb54d43d0dbd5d8a98cd
Travis Illig <tillig@paraesthesia.com>
2026-02-12 14:58:50 -0800

    Perf improvement - caching parameter-level Microsoft attribute lookup.

--- src/Autofac.Extensions.DependencyInjection/KeyedServiceMiddleware.cs
+++ src/Autofac.Extensions.DependencyInjection/KeyedServiceMiddleware.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Autofac Project. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using System.Collections.Concurrent;
 using System.Reflection;
 using Autofac.Core;
 using Autofac.Core.Resolving.Pipeline;
@@ -88,26 +89,17 @@ internal class KeyedServiceMiddleware : IResolveMiddleware
     private static ResolvedParameter CreateMicrosoftServiceKeyParameter(object serviceKey)
     {
         return new ResolvedParameter(
-            (p, c) =>
-            {
-                return Attribute.IsDefined(p, typeof(Microsoft.Extensions.DependencyInjection.ServiceKeyAttribute), inherit: true);
-            },
-            (p, c) =>
-            {
-                return KeyTypeManipulation.ChangeToCompatibleType(serviceKey, p.ParameterType, p);
-            });
+            (p, c) => ParameterAttributeCache.HasMicrosoftServiceKey(p),
+            (p, c) => KeyTypeManipulation.ChangeToCompatibleType(serviceKey, p.ParameterType, p));
     }
 
     private static ResolvedParameter CreateFromKeyedServicesParameter(object? requestedServiceKey)
     {
         return new ResolvedParameter(
+            (p, c) => ParameterAttributeCache.HasFromKeyedServicesAttribute(p),
             (p, c) =>
             {
-                return Attribute.IsDefined(p, typeof(FromKeyedServicesAttribute), inherit: true);
-            },
-            (p, c) =>
-            {
-                var filter = p.GetCustomAttribute<FromKeyedServicesAttribute>(inherit: true)!;
+                var filter = ParameterAttributeCache.GetFromKeyedServicesAttribute(p)!;
                 return filter.ResolveParameter(p, c, requestedServiceKey);
             });
     }
@@ -121,6 +113,41 @@ internal class KeyedServiceMiddleware : IResolveMiddleware
         catch (InvalidOperationException)
         {
             return null;
+        }
+    }
+
+    private static class ParameterAttributeCache
+    {
+        private static readonly ConcurrentDictionary<ParameterInfo, bool> MicrosoftServiceKeyAttributePresence = new();
+        private static readonly ConcurrentDictionary<ParameterInfo, FromKeyedServicesAttribute?> FromKeyedServicesAttributes = new();
+
+        public static bool HasMicrosoftServiceKey(ParameterInfo parameter)
+        {
+            if (parameter is null)
+            {
+                throw new ArgumentNullException(nameof(parameter));
+            }
+
+            return MicrosoftServiceKeyAttributePresence.GetOrAdd(
+                parameter,
+                static p => Attribute.IsDefined(p, typeof(Microsoft.Extensions.DependencyInjection.ServiceKeyAttribute), inherit: true));
+        }
+
+        public static bool HasFromKeyedServicesAttribute(ParameterInfo parameter)
+        {
+            return GetFromKeyedServicesAttribute(parameter) is not null;
+        }
+
+        public static FromKeyedServicesAttribute? GetFromKeyedServicesAttribute(ParameterInfo parameter)
+        {
+            if (parameter is null)
+            {
+                throw new ArgumentNullException(nameof(parameter));
+            }
+
+            return FromKeyedServicesAttributes.GetOrAdd(
+                parameter,
+                static p => p.GetCustomAttribute<FromKeyedServicesAttribute>(inherit: true));
         }
     }
 }
